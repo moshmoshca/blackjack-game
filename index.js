573caef273784fbc2beeeb8cc09058f702aa20dc
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-05-26 17:29:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+}
