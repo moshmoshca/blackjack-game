3a7439801b91016a300636aea9c596b61835fc3a
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-06-04 20:01:35 +0200

    Add getElementById for the displaying Cards numbers

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
     sumEl.textContent = `Sum: ${sum}`;
 
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Woohoo! You've got Blackjack!";
+        message = "You've got Blackjack!";
         hasBlackjack = true;
     } else {
         message = "You're out of the game!";
