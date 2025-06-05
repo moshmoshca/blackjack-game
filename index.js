fbed01ea1e20f9f51a60e8265e56fb9467c75598
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-06-05 15:50:29 +0200

    Create getRandomCard() and set it as the value of all cards

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 4;
-let secondCard = 10;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
@@ -8,6 +8,10 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+    return 5;
+}
 
 function startGame() {
     renderGame();
@@ -36,7 +40,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let newCard = 3;
+    let newCard = getRandomCard();
     sum += newCard;
     cards.push(newCard);
     renderGame();
