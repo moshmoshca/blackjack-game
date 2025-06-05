344ef034a2c09547e1110b4fa8c193e7b696bb09
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-06-05 17:37:53 +0200

    Add randomness to getRandomCard()

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-    return 5;
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
 }
 
 function startGame() {
