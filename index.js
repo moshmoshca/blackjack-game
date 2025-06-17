6e8cdc507a59378e86ded5263ce1ed15ff101f4f
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-06-17 13:46:32 +0200

    Add player information
    Display information about the player:
    - Player's name
    - Chip/Money quantity

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Mash",
+    chips: 500
+}
 let cards = [];
 let sum = 0;
 let hasBlackjack = false;
@@ -6,6 +10,9 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1;
