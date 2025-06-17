3056586d8972e32f8e2dd92afa70d2abd0b7a87e
Massiel Capellan <38220748+moshmoshca@users.noreply.github.com>
2025-06-17 09:51:39 +0200

    Move variables to startGame()
    According to the tutorial I moved the variables to the
    startGame() function. The goal is to avoid the program
    to run an select the first two cards before pressing
    the starGame button.
    
    It makes sense to do it that way. I wonder is the .push()
    would also have worked to use the empty cards array
    declared at the beginning.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -22,6 +20,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 }
 
